985a6b59733d52c04d23838a85e1b294b67c05c7
CR1STL <vadim-0505@inbox.ru>
2024-10-21 14:44:21 +0300

    0.7

--- Assets/Scripts/Bullet.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class Bullet : MonoBehaviour
-{
-    public float _speed;
-    private void Update()
-    {
-        transform.Translate(Vector2.right * Time.deltaTime * _speed);
-    }
-}
