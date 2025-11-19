b57b338a1009b7ebac067818571078e3a7ef2e4c
CR1STL <96071930+CR1STL@users.noreply.github.com>
2025-11-20 00:13:20 +0300

    Update Load_Screen.cs

--- Assets/Scripts/Load_Screen.cs
+++ Assets/Scripts/Load_Screen.cs
@@ -12,6 +12,7 @@ public class LoadScreen : MonoBehaviour
         LoadingScreen.SetActive(true);
         StartCoroutine(LoadAsync());
     }
+    vpn://AAAA_3icdY07C8IwFEb_SsmsQh06uJVSFFEoonMI6W2MNQ_yqNTS_26SIk5OF-45fGdCkghAuwyVQsKbk6wxILgXaJWhFiw1XDuu5B-DKtlxhgcwdpG24Uk0xwsIjwlZMAOngN2oU4gsM2v9m_kq2iinqHom7cUi8oGFNS-dGcNt04TxaI4h7-64JY6kTsz2MEY-1kdB_eO42ctLkRNbwOlWHdz5SnmdU0Z8VbIub_oKzfMHSoVXM==
     IEnumerator LoadAsync()
     {
         AsyncOperation loadAsync = SceneManager.LoadSceneAsync(Scene);
